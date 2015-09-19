2b41d95eec5a1caa8da08e4f7c9ce54368b25aa5
Tommy Brown <thomasbrown73095@gmail.com>
2015-09-18 19:42:33 -0500

    Initial commit

---
+++ Fuse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fuse
+//
+//  Created by Tommy Brown on 9/18/15.
+//  Copyright (c) 2015 tjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fuse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fuse
+//
+//  Created by Tommy Brown on 9/18/15.
+//  Copyright (c) 2015 tjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

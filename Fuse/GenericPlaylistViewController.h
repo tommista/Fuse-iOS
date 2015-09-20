68d575bc9859e207f1155bb39b299ead01a0348f
Tommy Brown <thomasbrown73095@gmail.com>
2015-09-19 21:38:21 -0500

    Added GenericPlaylistViewController

---
+++ Fuse/GenericPlaylistViewController.h
@@ -0,0 +1,13 @@
+//
+//  GenericPlaylistViewController.h
+//  Fuse
+//
+//  Created by Tommy Brown on 9/19/15.
+//  Copyright (c) 2015 tjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GenericPlaylistViewController : UIViewController
+
+@end
---
+++ Fuse/GenericPlaylistViewController.m
@@ -0,0 +1,21 @@
+//
+//  GenericPlaylistViewController.m
+//  Fuse
+//
+//  Created by Tommy Brown on 9/19/15.
+//  Copyright (c) 2015 tjl. All rights reserved.
+//
+
+#import "GenericPlaylistViewController.h"
+
+@interface GenericPlaylistViewController ()
+
+@end
+
+@implementation GenericPlaylistViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
